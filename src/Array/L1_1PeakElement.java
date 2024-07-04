bf1279c08c782bee94f2b75a31e9221741178ab9
SAI SUNIL AKURATHI <saisunilakurathi@gmail.com>
2024-07-03 20:31:55 -0400

    DataStructures

---
+++ src/Array/L1_1PeakElement.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Array;
+
+/**
+ * 
+ */
+public class L1_1PeakElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
